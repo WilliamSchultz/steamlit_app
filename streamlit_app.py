aa43cb6f290f21a1bd392a49e8e7a8516db74670
william <williamterrillschultz@gmail.com>
2024-04-26 19:44:01 +0200

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,17 @@ st.title("Grips product analysis")
 #st.sidebar.title('title!')
 st.sidebar.subheader('Filter Data')
 min_rev, max_rev = st.sidebar.slider('Select revenue range', min_value=df['rev'].min(), max_value=df['rev'].max(), value=(df['rev'].min(), df['rev'].max()))
+
 selected_brand = st.sidebar.selectbox('Select brand', df['brand'].unique())
+
 url_filter = st.sidebar.text_input('Enter URL to filter')
+
 selected_category = st.sidebar.selectbox('Select category', df['category'].unique())
+
+min_items, max_items = st.sidebar.slider('Select item range', min_value=df['items'].min(), max_value=df['items'].max(), value=(df['items'].min(), df['items'].max()))
+
+search_title = st.sidebar.text_input('Search by title')
+
 
 # Apply filters
 filtered_df = df[(df['rev'] >= min_rev) & (df['rev'] <= max_rev)]
@@ -42,6 +50,10 @@ if url_filter:
     filtered_df = filtered_df[filtered_df['url'].str.contains(url_filter)]
 if selected_category:
     filtered_df = filtered_df[filtered_df['category'] == selected_category]
+if min_items and max_items:
+    filtered_df = filtered_df[(filtered_df['items'] >= min_items) & (filtered_df['items'] <= max_items)]
+if search_title:
+    filtered_df = filtered_df[filtered_df['title'].str.contains(search_title)]
 
 # Display the dataframe in full browser size
 #st.sidebar.title('Sidebar Title')
