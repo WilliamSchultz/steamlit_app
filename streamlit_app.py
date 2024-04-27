5dec77055b25f41f47b41097020959863aefe201
william <williamterrillschultz@gmail.com>
2024-04-27 15:48:19 +0200

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,12 @@ with st.container():
     revenue_slider = col5.slider('Filter by Revenue', min_value=0, max_value=100000, value=(0, 100000))
     items_slider = col6.slider('Filter by Items', min_value=0, max_value=100, value=(0, 100))
 
+if st.button('Reset All Filters'):
+    search_title = ''
+    selected_brand = 'All Brands'
+    selected_category = 'All Categories'
+    url_filter = ''
+
 
 #st.set_option('theme.secondaryBackgroundColor', '#A670FF')
 #st.set_option('theme.accent', '#A670FF')
