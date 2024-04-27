7f68c063fae095e6e66d9d3d3861daa0cc274541
william <williamterrillschultz@gmail.com>
2024-04-27 15:34:19 +0200

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,13 @@ selected_brand = col2.selectbox('Select brand', df['brand'].unique())
 selected_category = col3.selectbox('Select category', df['category'].unique())
 url_filter = col4.text_input('Search URL')
 
+# Second row of filters in a container
+with st.container():
+    col5, col6, col7, col8 = st.columns(4)
+    revenue_slider = col5.slider('Filter by Revenue', min_value=0, max_value=100000, value=(0, 100000))
+    items_slider = col6.slider('Filter by Items', min_value=0, max_value=100, value=(0, 100))
+
+
 #st.set_option('theme.secondaryBackgroundColor', '#A670FF')
 #st.set_option('theme.accent', '#A670FF')
 
